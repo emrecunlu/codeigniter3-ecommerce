6fb894b16869eeef432a42d09e5f558d512612f8
unlcemre <unlcemre0@gmail.com>
2021-12-03 22:02:01 +0300

    Kullanıcı kayıt sayfası dinamik hale getirildi.

---
+++ application/views/register.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $this -> load -> view ('includes/links'); ?>
+</head>
+<body>
+<?php $this -> load -> view ('includes/header'); ?>
+<?php $this -> load -> view ('includes/navbar'); ?>
+    <div class="body">
+        <div class="container-fluid form-container">
+            <div class="user__form">
+                <div class="form__links">
+                    <ul>
+                        <li class="tab__link">
+                            <a href="<?=base_url('giris-yap')?>">
+                                Giriş Yap
+                            </a>
+                        </li>
+                        <li class="tab__link active">
+                            <a href="<?=base_url('kayit-ol')?>">
+                                Kayıt Ol
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="forms">
+                    <?php $this -> load -> view ('includes/msg_error.php'); ?>
+                    <div class="register__form form__user active">
+                        <form action="<?=base_url('user/register');?>" method="post">
+                            <div class="input__group">
+                                <label for="user-name">İsim Soyisim</label>
+                                <input type="text" name="user_name" id="user-name" placeholder="ad soyad" required>
+                            </div>
+                            <div class="input__group">
+                                <label for="user-mail">E-Mail</label>
+                                <input type="email" name="user_email" id="user-mail" placeholder="ad.soyad@example.com" required>
+                            </div>
+                            <div class="input__group">
+                                <label for="user-password">Şifre</label>
+                                <div class="show__input">
+                                    <input type="password" name="user_password" id="user-password" placeholder="****">
+                                    <i class="fa fa-eye show-password" aria-hidden="true" required></i>
+                                </div>
+                            </div>
+                            <div class="input__group">
+                                <label for="user-repassword">Şifre(Tekrar)</label>
+                                <div class="show__input">
+                                    <input type="password" name="user_repassword" id="user-repassword" placeholder="****">
+                                    <i class="fa fa-eye show-password" aria-hidden="true" required></i>
+                                </div>
+                            </div>
+                            <button type="submit" id="login-btn" name="login-btn">KAYIT OL</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php $this -> load -> view ('includes/scripts.php'); ?>
+    <script>
+        let showBtn = document.querySelectorAll('.show-password');
+
+        showBtn.forEach(btn => {
+            btn.addEventListener('click', e => {
+                let thisInput = e.target.closest('.show__input').children[0];
+
+                if (thisInput.getAttribute('type') === 'text') thisInput.setAttribute('type', 'password');
+                else {
+                    thisInput.setAttribute('type', 'text');
+                }
+            });
+        });
+    </script>
+</body>
+</html>
