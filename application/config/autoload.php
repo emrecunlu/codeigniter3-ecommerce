da9914d3c1fb01d40cb09f3b1268293339d33874
emrecunlu <unlcemre0@gmail.com>
2021-12-03 18:58:10 +0300

    Autoload ayarları yapıldı ve türkçe paket eklendi.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,10 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array(
+    'database',
+    'session',
+);
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +92,10 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url',
+    'form'
+);
 
 /*
 | -------------------------------------------------------------------
