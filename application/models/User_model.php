fc15b75470bd79971f3e0aabe4309b0f8b9f0cc4
unlcemre <unlcemre0@gmail.com>
2021-12-03 21:58:51 +0300

    Kullanıcı add () ve get () fonksiyonları eklendi.

---
+++ application/models/User_model.php
@@ -0,0 +1,39 @@
+<?php 
+
+    class User_model extends CI_Model
+    {
+
+        private $table_name = 'users';
+
+        public function __construct ()
+        {
+         
+            parent::__construct ();
+            
+        }
+
+        public function get ($where)
+        {
+
+            $user = $this -> db
+                          -> get_where ($this -> table_name, $where);
+
+            return $user;
+
+        }
+
+        public function add ($data)
+        {
+
+            $add_user = $this -> db
+                              -> insert ($this -> table_name, $data);
+
+            if ( $this -> db -> affected_rows () > 0 ) return true;
+
+            return false;
+
+        }
+
+    }
+
+?>
