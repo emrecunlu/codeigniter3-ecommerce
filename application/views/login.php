7fe3fa67d65383176b87df8aedf028f5489ab55a
emrecunlu <unlcemre0@gmail.com>
2021-12-05 14:24:23 +0300

    Kullanıcı çıkış mesajı eklendi.

--- application/views/login.php
+++ application/views/login.php
@@ -63,6 +63,30 @@
                 }
             });
         });
+
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
     </script>
+    <?php if ( $this -> session -> flashdata ('logout')) : ?>
+    <script>
+        toastr['error']('Güvenli ', '<?=$this -> session -> flashdata ("logout");?>')
+    </script>
+    <?php endif; ?>
 </body>
 </html>
